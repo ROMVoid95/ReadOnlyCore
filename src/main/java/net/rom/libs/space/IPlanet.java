457d657752fbbd5122fa33bbff6ab36de906bba8
ROMVoid95 <rom.void95@gmail.com>
2020-03-07 00:39:25 -0600

    Reduce interface to getPlanet only

--- src/main/java/net/rom/libs/space/IPlanet.java
+++ src/main/java/net/rom/libs/space/IPlanet.java
@@ -1,31 +1,9 @@
 package net.rom.libs.space;
 
-import java.util.ArrayList;
-
-import micdoodle8.mods.galacticraft.api.galaxies.SolarSystem;
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.biome.Biome;
+import micdoodle8.mods.galacticraft.api.galaxies.Planet;
 
 public interface IPlanet {
 	
-	String planetName();
-	
-	SolarSystem solarSystem();
-	
-	int DimensionId();
-	
-	int RequiredTier();
-	
-	float PlanetPhase(); 
-	
-	float PlanetSize();
-	
-	float DistanceFromCenter(); 
-	
-	float RelativeTime();
-	
-	ArrayList<Biome> biome();
-	
-	Class<? extends WorldProvider> WorldProvider();
+	Planet getPlanet();
 
 }
